babe7e3d64b6943fb3702660dc340ff90d23e88e
Luka <lukaguledani123@gmail.com>
2025-09-28 23:00:46 +0400

    unordered_map - non alphabetical

--- map_dictionary_cpp_fundamental/map_dictonary.cpp
+++ map_dictionary_cpp_fundamental/map_dictonary.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
-#include<map>
+#include <unordered_map>
 using namespace std;
 
 int main()
 {
-	map<string, string> my_dictionary;
+	unordered_map<string, string> my_dictionary;
 
-	my_dictionary.insert(pair<string, string>("Fruit", "Apple"));
-	
+	my_dictionary.insert(pair<string, string>("apple", "der apfel"));
+	my_dictionary.insert(pair<string, string>("orange", "die orange"));
+	my_dictionary.insert(pair<string, string>("banana", "die banane"));
+
 	for (auto& pair : my_dictionary)
 	{
-		cout << pair.first << " - " << pair.second;
+		cout << pair.first << " - " << pair.second << endl;
 		
 	}
 
