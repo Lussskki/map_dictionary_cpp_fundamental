fb4fd37ab7819043af8eb9bceef9a7bc11166850
Luka <lukaguledani123@gmail.com>
2025-09-28 22:50:39 +0400

    map to sort data

--- map_dictionary_cpp_fundamental/map_dictonary.cpp
+++ map_dictionary_cpp_fundamental/map_dictonary.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
+#include<map>
 using namespace std;
 
 int main()
 {
-	cout << "Hello World" << std::endl;
+	map<string, string> my_dictionary;
+
+	my_dictionary.insert(pair<string, string>("Fruit", "Apple"));
+	
+	for (auto& pair : my_dictionary)
+	{
+		cout << pair.first << " - " << pair.second;
+		
+	}
+
+	return 0;
 }
